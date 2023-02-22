9c1b498107b777de0862ec33ee1adc5e7b19d0b5
Antoni Pochopien <pochopienantek@gmail.com>
2023-02-22 12:41:13 +0100

    commit

--- lib/flutter_api.dart
+++ lib/flutter_api.dart
@@ -1,7 +1,7 @@
 library flutter_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Functions {
+  String helloWorld() {
+    return 'Hello World from Netigen Flutter API';
+  }
 }
