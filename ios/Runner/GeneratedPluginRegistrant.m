b16ae2b508cf98769b685441583a80b76aea64f7
JBTG <pochopienantek@gmail.com>
2023-06-09 13:40:07 +0200

    consentInfo update

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,42 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<google_mobile_ads/FLTGoogleMobileAdsPlugin.h>)
+#import <google_mobile_ads/FLTGoogleMobileAdsPlugin.h>
+#else
+@import google_mobile_ads;
+#endif
+
+#if __has_include(<in_app_purchase_storekit/InAppPurchasePlugin.h>)
+#import <in_app_purchase_storekit/InAppPurchasePlugin.h>
+#else
+@import in_app_purchase_storekit;
+#endif
+
+#if __has_include(<package_info/FLTPackageInfoPlugin.h>)
+#import <package_info/FLTPackageInfoPlugin.h>
+#else
+@import package_info;
+#endif
+
+#if __has_include(<webview_flutter_wkwebview/FLTWebViewFlutterPlugin.h>)
+#import <webview_flutter_wkwebview/FLTWebViewFlutterPlugin.h>
+#else
+@import webview_flutter_wkwebview;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTGoogleMobileAdsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMobileAdsPlugin"]];
+  [InAppPurchasePlugin registerWithRegistrar:[registry registrarForPlugin:@"InAppPurchasePlugin"]];
+  [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
+  [FLTWebViewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTWebViewFlutterPlugin"]];
+}
+
+@end
--- lib/ad_consent.dart
+++ lib/ad_consent.dart
@@ -3,29 +3,38 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class AdConsent {
   final params = ConsentRequestParameters();
 
-  Future<void> consentInfo() async {
+  Future<void> consentInfo({Function? onError, Function? showAd}) async {
     ConsentInformation.instance.requestConsentInfoUpdate(params, () async {
       if (await ConsentInformation.instance.isConsentFormAvailable()) {
-        loadForm();
+        loadForm(function: showAd ?? () {}, onError: onError ?? () {});
+      } else {
+        if (showAd != null) {
+          showAd();
+        }
       }
     }, (error) {
+      if (onError != null) {
+        onError();
+      }
       throw (error.message);
     });
   }
 
-  void loadForm() {
+  void loadForm({required Function function, required Function onError}) {
     ConsentForm.loadConsentForm(
       (ConsentForm consentForm) async {
         var status = await ConsentInformation.instance.getConsentStatus();
         if (status == ConsentStatus.required) {
           consentForm.show(
             (formError) {
-              loadForm();
+              loadForm(function: function, onError: onError);
             },
           );
         }
       },
       (FormError formError) {
+        onError();
+
         throw (formError.message);
       },
     );
@@ -45,8 +54,7 @@ class AdConsent {
     ConsentForm.loadConsentForm(
       (ConsentForm consentForm) async {
         var status = await ConsentInformation.instance.getConsentStatus();
-        if (status == ConsentStatus.notRequired ||
-            status == ConsentStatus.obtained) {
+        if (status == ConsentStatus.notRequired || status == ConsentStatus.obtained) {
           consentForm.show(
             (formError) {},
           );
