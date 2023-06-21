81aef0982f3c4aaa5b87ae620f378091eb6efd34
karol992 <karol.solek.programista@gmail.com>
2023-06-21 18:11:28 +0200

    [Add] in_app_purchase_storekit library.

--- lib/flutter_api.dart
+++ lib/flutter_api.dart
@@ -7,6 +7,8 @@ library flutter_api;
 
 export 'package:google_mobile_ads/google_mobile_ads.dart';
 export 'package:in_app_purchase/in_app_purchase.dart';
+export 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
+export 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
 
 export 'ad_consent.dart';
 export 'interstitial_ad.dart';
