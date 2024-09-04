265567808681209db4315f0beef330f13ed5ae68
Karol <k.solek@netigen.eu>
2024-09-05 00:41:04 +0200

    [Add] donate webview.

--- lib/flutter_api.dart
+++ lib/flutter_api.dart
@@ -11,8 +11,9 @@ export 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
 export 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
 
 export 'ad_consent.dart';
-export 'interstitial_ad.dart';
+export 'donate_web_view.dart';
 export 'initial_service_mixin.dart';
-export 'rewarded_ad.dart';
+export 'interstitial_ad.dart';
 export 'payment_service.dart';
+export 'rewarded_ad.dart';
 export 'survey_screen.dart';
